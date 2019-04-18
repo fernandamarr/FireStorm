56de6b751087965b4fbdf069789a5a0c475b7bb0
h-Benitez13 <hbenitez3711@gmail.com>
2019-04-18 15:13:52 -0400

    attempt routing

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -3,7 +3,7 @@ import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
-import { Input, FormBtn } from "../components/Form";
+import { Input } from "../components/Input";
 import SignUpBtn from "../components/SignUp";
 
 class SignUp extends Component {
