30cfd9f805446eb55ff54fb40f192fea1386115f
Hector Benitez <43594007+h-Benitez13@users.noreply.github.com>
2019-04-18 18:38:53 -0400

    working login, signup and game button (#4)

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,31 @@
 import React from "react";
-// import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import DevTeam from "./pages/DevTeam";
 // import Player from ".pages/Player";
-// import Login from "./pages/Login";
+import Login from "./pages/Login";
 import Game from './game';
-// import NoMatch from "./pages/NoMatch";
+import NoMatch from "./pages/NoMatch";
+import SignUp from "./pages/SignUp";
 // import SignUp from "./pages/SignUp";
 
 
 function App() {
-    return( 
-        // <Router>
-        //     <div>
-        //         <Login />
-        //         <Switch>
-        //             <Route exact path="/" component={<Login/>} />
-        //             <Route exact path="/game" component={<Game/>}/>
-        //             <Route exact path="/signup" component={<SignUp/>} />
-        //             <Route component={NoMatch}/>
-        //         </Switch>
-                
-                <Game/>
-                /* <Switch>
-                    <Route exact path="/player" component={<Player/>}/>
-                    <Route exact path="/devteam" component = {<DevTeam/>}/>
-                </Switch> */
-        //     </div>
-        // </Router>
-    )
+  return (
+    <Router>
+      <div>
+
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/game" component={Game} />
+          <Route exact path="/signup" component={SignUp} />
+          {/* <Route exact path="/player" component={<Player />} /> */}
+          {/* <Route exact path="/devteam" component={<DevTeam />} /> */}
+          <Route component={NoMatch} />
+        </Switch >
+      </div>
+    </Router>
+  )
+  // <Game/>
 
 }
 
