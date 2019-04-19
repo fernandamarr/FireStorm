a3ec4c5665b6d78656b18c2d8a1ac797fd41d4fa
h-Benitez13 <hbenitez3711@gmail.com>
2019-04-19 07:14:28 -0400

    login page styled, looking for fonts to match theme

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
+      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center", marginTop: "100px" }}
+      className="jumbotron bg-transparent"
     >
       {children}
     </div>
