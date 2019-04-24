2ca77657c5d45c78f5932f82cc573311b9305493
Mykola Duffy <mpduffy82@gmail.com>
2019-04-24 13:50:20 -0400

    added flames

--- client/src/game/index.js
+++ client/src/game/index.js
@@ -41,8 +41,8 @@ export default class App extends Component {
   startGame() {
     // eslint-disable-next-line    
     let config = {
-      width: 1900,
-      height: 1067,
+      width: 1280,
+      height: 720,
       type: Phaser.AUTO,
       pixelArt: true,
       physics: {
