3d73e30305329070632a9064bdb49268baf1ef92
h-Benitez13 <hbenitez3711@gmail.com>
2019-04-26 11:55:48 -0400

    size adjusted

--- client/src/components/SignUpGrid/index.js
+++ client/src/components/SignUpGrid/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-let aesthetic = {
-    backgroundImage: 'linear-gradient(to top left, rgb(66, 223, 244), rgb(9, 36, 237), rgb(129, 1, 193), rgb(151, 1, 193), rgb(193, 1, 186))',
-    height: '900px'
-}
 
-let warriorImg = {
+const aesthetic= {
+    backgroundImage: 'linear-gradient(to top left, rgb(66, 223, 244), rgb(9, 36, 237), rgb(129, 1, 193), rgb(151, 1, 193), rgb(193, 1, 186))',
+    height: "800px"
+}
+const warriorImg = {
     backgroundImage: 'url(https://art.ngfiles.com/images/695000/695547_michafrario_super-pocket-fighter-nova.gif?f1543967599)',
     backgroundPosition: 'bottom right',
     backgroundRepeat: 'no-repeat',
@@ -12,12 +12,12 @@ let warriorImg = {
 }
 // Exporting the Container, Row, and Col components from this file
 export const Container = ({children })=> {
-    return <div className="container-fluid"style={aesthetic} >{children}</div>;
+    return <div  className="container-fluid" style={aesthetic}>{children}</div>;
 }
 
 // This Container component allows us to use a bootstrap container without worrying about class names
 export const Row = ({ children }) => {
-    return <div className="row" style={warriorImg}>{children} </div>;
+    return <div  className="row" style={warriorImg} >{children} </div>;
 }
 
 
@@ -30,6 +30,7 @@ export const Col = ({ size, children }) => {
                 .split(" ")
                 .map(size => "col-" + size)
                 .join(" ")}
+                
         >
             {children}
         </div>
