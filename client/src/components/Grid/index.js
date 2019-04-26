471794a2558ef9d300a5fb6fff7748e679274e7c
h-Benitez13 <hbenitez3711@gmail.com>
2019-04-26 12:07:00 -0400

    adjusted sizing for login/signup

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-
+let aesthBack = {
+    height: '800px'
+}
 // Exporting the Container, Row, and Col components from this file
 export const Container = ({children}) => {
-    return <div className="container-fluid "  >{children}</div>;
+    return <div id="loginContain" className="container-fluid "  >{children}</div>;
 }
 
 // This Container component allows us to use a bootstrap container without worrying about class names
 export const Row = ({children}) => {
-    return <div className= "row "  >{children}</div>;
+    return <div className= "row " style={aesthBack}  >{children}</div>;
 }
 
 
--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{  clear: "both", textAlign: "center", marginTop: "100px", marginBottom: "-10px" }}
+      style={{  clear: "both", textAlign: "center", marginTop: "200px", marginBottom: "-40px" }}
       className="jumbotron bg-transparent"
     >
       {children}
